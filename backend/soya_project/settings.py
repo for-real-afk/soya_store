5723229f3d1418a8366691df064b469d8f97132a
Ankit Tiwari <tiwari.ankit0804@gmail.com>
2025-05-13 23:49:59 +0530

    changes

--- backend/soya_project/settings.py
+++ backend/soya_project/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'soya_project.middleware.SecurityMiddleware',  # Custom security middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # CORS Settings
@@ -159,6 +160,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Static files configuration
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '..', 'frontend', 'build', 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
